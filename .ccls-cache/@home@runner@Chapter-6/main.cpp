c2cb692f16476beb0bcde93681ba4b6b99151982
Dione099 <dionegeorge.dg@gmail.com>
2024-12-17 19:07:20 +0000

    program completed

--- .ccls-cache/@home@runner@Chapter-6/main.cpp
+++ .ccls-cache/@home@runner@Chapter-6/main.cpp
@@ -1,59 +1,28 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath> // For ceil
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
 using namespace std;
 
-// Constants
-const double COVERAGE_PER_GALLON = 110.0;
-const double HOURS_PER_GALLON = 8.0;
-const double LABOR_COST_PER_HOUR = 25.0;
-
-// Function prototypes
-double getValidatedInput(string prompt, double min);
-void calculateAndDisplayResults(int numRooms, double paintPrice);
-
 int main() {
-    int numRooms = getValidatedInput("Enter the number of rooms to be painted: ", 1);
-    double paintPrice = getValidatedInput("Enter the price of paint per gallon (minimum $10.00): ", 10.0);
+    srand(time(0)); // Seed random number generator
+    int randomNumber = rand() % 100 + 1; // Random number between 1 and 100
+    int guess;
+    int guessCount = 0;
 
-    calculateAndDisplayResults(numRooms, paintPrice);
-    return 0;
-}
+    cout << "Guess the number between 1 and 100!\n";
 
-// Function to get validated input
-double getValidatedInput(string prompt, double min) {
-    double value;
+    // Loop until the correct guess
     do {
-        cout << prompt;
-        cin >> value;
-        if (value < min) {
-            cout << "Invalid input! Value must be at least " << min << ".\n";
-        }
-    } while (value < min);
-    return value;
-}
+        cout << "Enter your guess: ";
+        cin >> guess;
+        guessCount++;
 
-// Function to calculate and display final results
-void calculateAndDisplayResults(int numRooms, double paintPrice) {
-    double totalSquareFootage = 0.0, squareFootage;
+        if (guess > randomNumber)
+            cout << "Too high! Try again.\n";
+        else if (guess < randomNumber)
+            cout << "Too low! Try again.\n";
+    } while (guess != randomNumber);
 
-    for (int i = 1; i <= numRooms; i++) {
-        squareFootage = getValidatedInput("Enter square footage for room " + to_string(i) + ": ", 0.0);
-        totalSquareFootage += squareFootage;
-    }
-
-    int gallonsRequired = ceil(totalSquareFootage / COVERAGE_PER_GALLON);
-    double laborHours = gallonsRequired * HOURS_PER_GALLON;
-    double paintCost = gallonsRequired * paintPrice;
-    double laborCost = laborHours * LABOR_COST_PER_HOUR;
-    double totalCost = paintCost + laborCost;
-
-    cout << fixed << setprecision(2);
-    cout << "\nPaint Job Estimate:\n";
-    cout << "-------------------\n";
-    cout << "Gallons of paint required: " << gallonsRequired << endl;
-    cout << "Hours of labor required: " << laborHours << endl;
-    cout << "Cost of paint: $" << paintCost << endl;
-    cout << "Labor charges: $" << laborCost << endl;
-    cout << "Total cost: $" << totalCost << endl;
+    cout << "Congratulations! You guessed the number in " << guessCount << " tries.\n";
+    return 0;
 }
